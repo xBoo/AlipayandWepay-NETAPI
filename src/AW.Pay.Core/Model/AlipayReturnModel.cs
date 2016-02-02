1d48a0757f8e7c7f4eda625e5c92679f88ca4eb2
xboo <xubo@guanaihui.com>
2016-02-02 17:24:26 +0800

    增加注释

--- src/AW.Pay.Core/Model/AlipayReturnModel.cs
+++ src/AW.Pay.Core/Model/AlipayReturnModel.cs
@@ -8,12 +8,24 @@ namespace AW.Pay.Core.Model
 {
     public class AlipayReturnModel
     {
+        /// <summary>
+        /// 商户订单号
+        /// </summary>
         public string OutTradeNo { get; set; }
 
+        /// <summary>
+        /// 交易订单号
+        /// </summary>
         public string TradeNo { get; set; }
 
+        /// <summary>
+        /// 交易总金额
+        /// </summary>
         public decimal TotalFee { get; set; }
 
+        /// <summary>
+        /// 交易状态
+        /// </summary>
         public string TradeStatus { get; set; }
     }
 }
--- src/AW.Pay.Core/Model/WepayReturnModel.cs
+++ src/AW.Pay.Core/Model/WepayReturnModel.cs
@@ -8,14 +8,29 @@ namespace AW.Pay.Core.Model
 {
     public class WepayReturnModel
     {
+        /// <summary>
+        /// 商户订单号
+        /// </summary>
         public string OutTradeNo { get; set; }
 
+        /// <summary>
+        /// 交易订单号
+        /// </summary>
         public string TradeNo { get; set; }
 
+        /// <summary>
+        /// 交易金额（单位元）
+        /// </summary>
         public decimal TotalFee { get; set; }
 
+        /// <summary>
+        /// 交易状态
+        /// </summary>
         public string TradeStatus { get; set; }
 
+        /// <summary>
+        /// 返回xml
+        /// </summary>
         public string ReturnXml { get; set; }
     }
 }
